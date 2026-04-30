2938527f0d43a7e82e71963c28801bfde1e28c54
Koštíř Miloslav <miloslav.kostir@gmail.com>
2026-04-30 12:40:57 +0200

    Tests - diferences between latte 3.0 and 3.1

--- tests/ImplementationTest.php
+++ tests/ImplementationTest.php
@@ -6,7 +6,7 @@ class ImplementationTest extends TestCase
 {
     public function test_latte_implemented(): void
     {
-        $output = view('implementation/implemented', ['foo' => 'Bar'])->render();
+        $output = view('implementation/implemented', ['foo' => 'Bar', 'title' => null])->render();
 
         $expected = $this->getExpected('implemented');
 
